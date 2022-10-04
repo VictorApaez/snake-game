934595ab2373c65309d4fbb985a447cbbd5e7b27
Victor Apaez <vic.apaez@gmail.com>
2022-10-04 12:42:37 -0400

    created basic styles

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ let gameContainer = document.querySelector(".game");
 
 for (let i = 0; i < 100; i++) {
   let div = document.createElement("div");
-  div.classList.add(`data-type="${i}"`);
+  div.dataset.index = i;
   div.classList.add(`cell`);
   gameContainer.append(div);
 }
+
+let startingDiv = document.querySelector(`[data-index="35"]`);
+startingDiv.style.backgroundColor = "green";
