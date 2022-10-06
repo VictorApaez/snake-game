1c79211939d2597ffc1c81ababf8d6fed50e0794
Victor Apaez <vic.apaez@gmail.com>
2022-10-05 21:35:19 -0400

    created apple at random location

--- script2.js
+++ script2.js
@@ -1,24 +1,45 @@
 let board = document.querySelector(".board");
-let boardWidth = 25 + 1;
-board.style.gridTemplateColumns = `repeat(${boardWidth - 1}, 1fr)`;
-board.style.gridTemplateRows = `repeat(${boardWidth - 1}, 1fr)`;
+let boardWidth = 15;
+let frameRate = 500;
+board.style.gridTemplateColumns = `repeat(${boardWidth}, 1fr)`;
+board.style.gridTemplateRows = `repeat(${boardWidth}, 1fr)`;
 let midBoard = Math.ceil(boardWidth / 2);
 
-let snake = [
-  { x: midBoard, y: 4 },
-  { x: midBoard, y: 3 },
-  { x: midBoard, y: 2 },
-  { x: midBoard, y: 1 },
-];
-let apple = [{ x: 3, y: 3 }];
-
+let snake = [{ x: midBoard, y: 1 }];
+let apple = { x: 1, y: 1 };
 let direction = { x: 0, y: 1 };
+
+// ================ APPLE LOCATION =====================
+function randomAppleLocation() {
+  let num1 = Math.floor(Math.random() * boardWidth + 1);
+  let num2 = Math.floor(Math.random() * boardWidth + 1);
+  apple.x = num1;
+  apple.y = num2;
+  let approved = false;
+  for (let i = 0; i < snake.length; i++) {
+    if (apple.x === snake[i].x) {
+      for (let j = 0; j < snake.length; j++) {
+        if (apple.y === snake[j].y) {
+          approved = false;
+        } else approved = true;
+      }
+    } else approved = true;
+  }
+  if (approved) {
+    return apple;
+  } else {
+    randomAppleLocation();
+  }
+}
+randomAppleLocation();
+
+// ================ ANIMATION =====================
 function animate() {
   if (
-    snake[0].x < boardWidth &&
+    snake[0].x < boardWidth + 1 &&
     snake[0].x > 0 &&
     snake[0].y > 0 &&
-    snake[0].y < boardWidth
+    snake[0].y < boardWidth + 1
   ) {
     board.innerHTML = "";
     drawSnake();
@@ -26,13 +47,14 @@ function animate() {
     update();
     setTimeout(() => {
       requestAnimationFrame(animate);
-    }, 200);
+    }, frameRate);
   } else {
     console.log("You lost my guy");
   }
 }
 animate();
 
+// ================ UPDATE =====================
 function update() {
   for (let i = snake.length - 1; i > 0; i--) {
     snake[i] = { ...snake[i - 1] };
@@ -40,7 +62,7 @@ function update() {
   snake[0].y += direction.y;
   snake[0].x += direction.x;
 }
-
+// ================ DRAW SNAKE =====================
 function drawSnake() {
   snake.forEach((section) => {
     let snakeElement = document.createElement("div");
@@ -50,15 +72,15 @@ function drawSnake() {
     board.appendChild(snakeElement);
   });
 }
-
+// ================ DRAW APPLE =====================
 function drawApple() {
   let appleElement = document.createElement("div");
   appleElement.classList.add("apple");
-  appleElement.style.gridColumnStart = apple[0].x;
-  appleElement.style.gridRowStart = apple[0].y;
+  appleElement.style.gridColumnStart = apple.x;
+  appleElement.style.gridRowStart = apple.y;
   board.append(appleElement);
 }
-
+// ================ EVENT LISTENERS =====================
 addEventListener("keydown", (e) => {
   switch (e.key) {
     case "ArrowUp":
